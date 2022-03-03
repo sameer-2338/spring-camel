ba8fd4f70f0547020b5f8e914f9e9004b50ef77b
sameer2338-java <85013063+sameer2338-java@users.noreply.github.com>
2022-03-03 11:24:28 +0530

    Add files via upload

---
+++ camel-mq/src/main/java/com/org/camel/camelmq/CamelMqApplication.java
@@ -0,0 +1,13 @@
+package com.org.camel.camelmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMqApplication.class, args);
+	}
+
+}
---
+++ camel-mq/src/test/java/com/org/camel/camelmq/CamelMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.camel.camelmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
